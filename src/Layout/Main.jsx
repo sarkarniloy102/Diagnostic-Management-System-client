a5af4251a3974e9771e811b008e74c63bb140f84
sarkar niloy <sarkarniloy102@gmail.com>
2024-03-17 01:13:05 +0600

    footer section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
+    
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
